5c24fac215a7d2ed62fd86358af898f299260207
Aastha <aastha@192.168.1.3>
2025-11-06 16:36:18 +0530

    modified files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
+import { LoanState } from './app/state/loan.state';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(NgxsModule.forRoot([LoanState]))
+  ]
+});
+  
