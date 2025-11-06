a1dcd947cf84be90e2dff883131521f1a76b72be
Aastha <aastha@192.168.1.3>
2025-11-07 01:18:48 +0530

    Modified files to store data over local storage

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { NgxsModule } from '@ngxs/store';
 import { LoanState } from './app/state/loan.state';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(NgxsModule.forRoot([LoanState]))
+    importProvidersFrom(
+      NgxsModule.forRoot([LoanState]),
+      NgxsStoragePluginModule.forRoot({keys : ['loans'] })
+    )
   ]
 }).catch(err => console.error(err));
   
